bce3afdbe500408ff4261fa33d255e3464711b28
Faizan80235 <fa3031980@gmail.com>
2025-02-06 03:02:21 +0500

    Add react Toastify on Registerd page

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Form, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import { handelerror, handelSucees } from '../Utils/Utils';
+
 function Login() {
+  const [info, setInfo] = useState({
+    email: '',
+    password: '',
+    contactNumber: '',
+    chatWith: ''
+  });
+
+  const handleChange = (e) => {
+    setInfo({ ...info, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    if (!info.email || !info.email || !info.contactNumber  || !info.password || !info.chatWith) {
+      handelerror("Error while Registering");
+    }
+    else {
+      handelSucees("Registerd")
+    }
+    e.preventDefault();
+    console.log(info); 
+  };
 
   return (
     <div style={{ backgroundColor: '#003366', minHeight: '100vh' }}>
       <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
-        <Form className="p-4" style={{ backgroundColor: '#003366', borderRadius: '8px', width: '100%', maxWidth: '400px' }}>
+        <Form className="p-4" style={{ backgroundColor: '#003366', borderRadius: '8px', width: '100%', maxWidth: '400px' }} onSubmit={handleSubmit}>
           <h2 className="text-white text-center mb-4">Welcome To Chatrix</h2>
 
           <Form.Group className="mb-3">
@@ -15,7 +38,8 @@ function Login() {
               type="email" 
               placeholder="Enter email" 
               name="email" 
-              required
+              value={info.email} 
+              onChange={handleChange} 
             />
           </Form.Group>
 
@@ -25,7 +49,8 @@ function Login() {
               type="password" 
               placeholder="Password" 
               name="password" 
-              required
+              value={info.password} 
+              onChange={handleChange} 
             />
           </Form.Group>
 
@@ -35,7 +60,8 @@ function Login() {
               type="text" 
               placeholder="Enter contact number" 
               name="contactNumber" 
-              required
+              value={info.contactNumber} 
+              onChange={handleChange} 
             />
           </Form.Group>
 
@@ -44,19 +70,17 @@ function Login() {
             <Form.Control 
               type="text" 
               placeholder="Enter the person to chat with" 
-              name="chatWith"
+              name="chatWith" 
+              value={info.chatWith} 
+              onChange={handleChange} 
             />
           </Form.Group>
 
           <Button variant="primary" type="submit" className="w-100">
-            <Link to='/index/Chat/start/welcome/Chatrix' variant="primary" type="submit" className="w-100"style={{color
-              :"white",textDecoration:"none"
-            }} >
             Submit
-            
-            </Link>
           </Button>
         </Form>
+        <ToastContainer />
       </Container>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';  // Bootstrap styles
 import 'flowbite/dist/flowbite.css';  // Flowbite styles
+import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
