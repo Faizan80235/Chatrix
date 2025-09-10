0b2cd011f4eab064b9b81d7699734d3291bc2372
Faizan80235 <fa3031980@gmail.com>
2025-09-11 01:25:26 +0500

    change api path url

--- src/components/config/Api.js
+++ src/components/config/Api.js
@@ -1,9 +1,9 @@
 // API Configuration
 export const API_BASE_URL =
-  process.env.REACT_APP_API_URL || 'https://chatrix-backend.vercel.app';
+  process.env.REACT_APP_API_URL || 'http://localhost:5000/';
 
 export const SOCKET_URL =
-  process.env.REACT_APP_SOCKET_URL || 'https://chatrix-backend.vercel.app';
+  process.env.REACT_APP_SOCKET_URL || 'http://localhost:5000/';
 
 // API Endpoints
 export const API_ENDPOINTS = {
